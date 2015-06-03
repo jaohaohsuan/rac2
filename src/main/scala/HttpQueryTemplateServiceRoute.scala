187e26aae1761b1cbcfebf685c3c4a0b71787d57
henry <jaohaohsuan@gmail.com>
2015-06-03 23:58:59 +0800

    refactoring queryTemplate API

--- src/main/scala/HttpQueryTemplateServiceRoute.scala
+++ src/main/scala/HttpQueryTemplateServiceRoute.scala
@@ -2,7 +2,6 @@
  * Created by henry on 6/1/15.
  */
 
-import org.apache.lucene.search.BooleanClause.Occur
 import spray.routing.PathMatcher.{ Unmatched, Matched, Matching }
 import spray.routing._
 import spray.http.StatusCodes._
@@ -18,10 +17,12 @@ import net.hamnaberg.json.collection._
 import shapeless._
 
 case class MatchClause(query: String, operator: String, occur: String)
+
 case class SpanNearClause(query: String, slop: Int, inOrder: Boolean, occur: String)
+
 case class NamedClause(uri: String, occur: String)
 
-case class Clause(title: String)
+case class ClauseTitle(title: String)
 
 trait HttpQueryTemplateServiceRoute extends HttpService {
 
@@ -39,82 +40,81 @@ trait HttpQueryTemplateServiceRoute extends HttpService {
     "spanNearClause"
   }
 
-  val clauseRegex = """^match$|^near$|^named$""".r
+  val clauseTypeRegex = """^match$|^near$|^named$""".r
   val occurRegex = """^must$|^must_not$|^should$""".r
 
-  val queryTemplatePathMatcher = Segment / "template" / "_query" / Segments
+  val clauseItemPathMatcher = clauseTypeRegex / IntNumber
 
-  object _query extends PathMatcher1[String] {
-    def apply(path: Path) = {
-
-      val pathString = path.toString
-      """^[^_]*""".r.findFirstIn(path.toString) match {
-        case Some(s) => Matched(Path(pathString.substring(s.length)), s :: HNil)
+  object Underline extends PathMatcher1[String] {
+    def apply(path: Path) =
+      """^[^_]*""".r.findPrefixMatchOf(path.toString) match {
+        case Some(m) => Matched(Path(m.after.toString), m.matched :: HNil)
         case None => Unmatched
       }
-    }
   }
 
-  def queryTemplateRoute = pathPrefix(_query ~ "_query" / "template" / Segment) { (realm, id) =>
-
-    complete(id)
-
-
-  }
-
-  def queryRoute =
-
+  def queryTemplateRoute = pathPrefix(Underline ~ "_query" / "template" / Segment) { (realm, id) =>
     respondWithMediaType(`application/vnd.collection+json`) {
       URI { href =>
+        get {
+          pathEndOrSingleSlash {
+            val links = List(
+              Link(href.resolve(s"${href.getPath}/must"), "section"),
+              Link(href.resolve(s"${href.getPath}/must_not"), "section"),
+              Link(href.resolve(s"${href.getPath}/should"), "section"),
+              Link(href.resolve(s"${href.getPath}/match"), "edit"),
+              Link(href.resolve(s"${href.getPath}/near"), "edit"),
+              Link(href.resolve(s"${href.getPath}/named"), "edit"))
 
-        pathSuffix(queryTemplatePathMatcher) { (id, segs) =>
-
-          val links = List(
-            Link(href.resolve(s"${href.getPath}/must"), "section"),
-            Link(href.resolve(s"${href.getPath}/must_not"), "section"),
-            Link(href.resolve(s"${href.getPath}/should"), "section"),
-            Link(href.resolve(s"${href.getPath}/clauses/match"), "edit"),
-            Link(href.resolve(s"${href.getPath}/clauses/near"), "edit"),
-            Link(href.resolve(s"${href.getPath}/clauses/named"), "edit"))
-
-          complete(JsonCollection(href, links, List.empty))
-        } ~
-          pathSuffix(occurRegex / queryTemplatePathMatcher) { (occur: String, id: String, segments: List[String]) =>
-
-            val sampleItem = Item(href.resolve(s"${href.getPath}/match/32341"), Clause("基金 沪指 风险"), List.empty)
-
-            complete(JsonCollection(href, List.empty, List(sampleItem)))
+            complete(JsonCollection(href, links, List.empty))
           } ~
-          pathSuffix(clauseRegex / "clauses" / queryTemplatePathMatcher) { (clause: String, id: String, segments: List[String]) =>
-
-            get {
-              val template: Option[Template] = clause match {
+            path(occurRegex) { occur =>
+              //query clauses of an occur
+              val sampleItems = List(Item(href.resolve(s"${href.getPath.dropRight(occur.length)}match/32341"), ClauseTitle("基金 沪指 风险"), List.empty))
+              complete(JsonCollection(href, List.empty, sampleItems))
+            } ~
+            path(clauseTypeRegex) { clauseType =>
+              //shows template data of each clause type
+              val template: Option[Template] = clauseType match {
                 case "match" => MatchClause("", "AND", "")
                 case "near" => SpanNearClause("", 1, false, "")
                 case "named" => NamedClause("", "")
               }
               complete(JsonCollection(href, List.empty, List.empty, List.empty, template))
             } ~
-              post {
-                handleWith(appendMatchClause(id)) ~ handleWith(appendSpanNearClause)
-              }
-          } ~
-          pathSuffix(IntNumber / clauseRegex / occurRegex / queryTemplatePathMatcher) { (clauseId: Int, clause: String, occur: String, id: String, segments: List[String]) =>
-            get {
-              val clauseObj = clause match {
+            path(clauseItemPathMatcher) { (clauseType, clauseId) =>
+              //details of a clause
+              val clauseObj = clauseType match {
                 case "match" => MatchClause("", "AND", "")
                 case "near" => SpanNearClause("", 1, false, "")
                 case "named" => NamedClause("", "")
               }
               complete(JsonCollection(href.resolve(""), List.empty, Item(href, clauseObj, List.empty)))
-            } ~ post {
-              complete("updated")
-            } ~ delete {
-              complete("deleted")
             }
-
+        } ~
+          post {
+            path(clauseTypeRegex) { clauseType =>
+              //add a new clause
+              handleWith(appendMatchClause(id)) ~ handleWith(appendSpanNearClause)
+            }
+          } ~
+          put {
+            path(clauseItemPathMatcher) { (clauseType, clauseId) =>
+              //update a clause
+              handleWith(appendMatchClause(id)) ~ handleWith(appendSpanNearClause)
+            }
+          } ~
+          delete {
+            path(clauseItemPathMatcher) { (clauseType, clauseId) =>
+              //delete a clause
+              handleWith(appendMatchClause(id)) ~ handleWith(appendSpanNearClause)
+            } ~
+              path(occurRegex) { occur =>
+                //remove all clause
+                complete(OK)
+              }
           }
       }
-
     }
+  }
 }
